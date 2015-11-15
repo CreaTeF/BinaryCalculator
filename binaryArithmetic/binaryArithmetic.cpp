b600155066375b2943c2cdd93426df1708fba0ec
CreaTeF <eiby.lamaravilla.2010@hotmail.com>
2015-11-15 14:28:01 -0500

    Update binaryArithmetic.cpp

--- binaryArithmetic/binaryArithmetic.cpp
+++ binaryArithmetic/binaryArithmetic.cpp
@@ -22,6 +22,6 @@ std::string binaryConversion(std::string binary)
 		}
 	}
 
-	//it converts a double týpe to string.
+	//it converts a double type to string.
 	return std::to_string((int)decimal);
 }
